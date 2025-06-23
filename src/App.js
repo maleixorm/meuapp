98a57d3f9d202227d6c4f371838f8380c7e4fcee
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-23 17:17:44 -0300

    Utilizando os ciclos de vida dos components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 class App extends Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            hora: '00:00:00'
+        };
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({hora: new Date().toLocaleTimeString()})
+        }, 1000);
+    }
+
+    componentDidUpdate() {
+        console.log('Atualizou!');
+    }
+    
     render() {
         return(
             <div>
-                <h1>Meu Projeto</h1>
+                <h1>Meu Projeto {this.state.hora}</h1>
             </div>
         );
     }
