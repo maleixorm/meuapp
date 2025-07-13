68af79050c73b40538493731ab08aba08eca1909
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-13 13:10:52 -0300

    Trabalhando com React Hooks

--- src/App.js
+++ src/App.js
@@ -1,54 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class App extends Component{
+function App() {
     
-    constructor(props){
-        super(props);
-        this.state = {
-            form: {
-                nome: '',
-                email: '',
-                senha: '',
-                sexo: ''
-            }
-        };
-        this.dadosForm = this.dadosForm.bind(this);
-    }
+    const [tarefas, setTarefas] = useState([
+        'Pagar a conta de luz',
+        'Estudar React Hooks'
+    ]);
 
-    dadosForm(e){
-        let form = this.state.form;
-        form[e.target.name] = e.target.value;
-        this.setState({form: form});
+    const [nome, setNome] = useState('Marcos');
+
+    const [input, setInput] = useState('');
+
+    function handleAdd() {
+        setTarefas([...tarefas, input]);
+        setInput(''); 
     }
     
-    render(){
-        return(
-            <div>
-                <h2>Login</h2>
-                <label for="nome">Nome:</label>
-                <input type="text" name="nome" value={this.state.form.nome} onChange={this.dadosForm}/>
-                <br/><br/>
-                <label for="email">Email:</label>
-                <input type="email" name="email" value={this.state.form.email} onChange={this.dadosForm}/>
-                <br/><br/>
-                <label for="password">Senha:</label>
-                <input type="password" name="senha" value={this.state.form.senha} onChange={this.dadosForm} />
-                <br/><br/>
-                <label for="sexo">Sexo:</label>
-                <select name="sexo" value={this.state.form.sexo} onChange={this.dadosForm}>
-                    <option value="masculino">Masculino</option>
-                    <option valie="feminino">Feminino</option>
-                </select>
-                
-                <div>
-                    <h4>{this.state.form.nome}</h4>
-                    <h4>{this.state.form.email}</h4>
-                    <h4>{this.state.form.senha}</h4>
-                    <h4>{this.state.form.sexo}</h4>
-                </div>
-            </div>
-        );
-    }
+    return(
+        <div>
+            <h1>Hooks</h1>
+            <ul>
+                {tarefas.map(tarefa => (
+                    <li key={tarefa}>{tarefa}</li>
+                ))}
+            </ul>
+            <input type='text' value={input} onChange={ e => setInput(e.target.value) } />
+            <button type='button' onClick={handleAdd}>Adicionar</button>
+            <h2>{nome}</h2>
+        </div>
+    );
 }
 
 export default App;
